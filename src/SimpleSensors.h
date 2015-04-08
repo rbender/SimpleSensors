3bfb5178e4ea4c92fcc688496a798991f6d2d63e
Rob Bender <rbender@gmail.com>
2015-04-08 11:15:26 -0400

    Migrate to Arduino 1.5 library structure.

---
+++ src/SimpleSensors.h
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+
+#ifndef SimpleSensors_h
+#define SimpleSensors_h
+
+#include "core/SimpleSensorsBase.h"
+#include "core/SensorImplementations.h"
+#include "core/SensorCollection.h"
+#include "core/Emitters.h"
+
+#endif
